45d6ad37fcebec4473d617aa13f1c59dfda7ea2f
jenna <shinysuh1992@gmail.com>
2024-02-25 18:09:59 +0900

    feat: username sign up 페이지 구현

---
+++ lib/features/authentication/username_screen.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/authentication/common/form_button.dart';
+
+class UsernameScreen extends StatefulWidget {
+  const UsernameScreen({super.key});
+
+  @override
+  State<UsernameScreen> createState() => _UsernameScreenState();
+}
+
+class _UsernameScreenState extends State<UsernameScreen> {
+  String _username = '';
+
+  final TextEditingController _usernameController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState(); // at the beginning of everything
+    _usernameController.addListener(() {
+      setState(() {
+        _username = _usernameController.text;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    /*
+       위젯이 사라질 때 _usernameController dispose
+       메모리를 위패 필수 => 잊었을 경우 eventually 앱이 crash 됨
+       This removes all the eventListeners
+    */
+    _usernameController.dispose();
+    super.dispose(); // better do this at the end => cleaning up
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Sign up',
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: Sizes.size36),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Gaps.v40,
+            const Text(
+              'Create Username',
+              style: TextStyle(
+                fontSize: Sizes.size20 + Sizes.size2,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            Gaps.v6,
+            const Text(
+              'You can always change this later.',
+              style: TextStyle(
+                color: Colors.black54,
+                fontSize: Sizes.size16,
+              ),
+            ),
+            Gaps.v36,
+            TextField(
+              controller: _usernameController,
+              cursorColor: Theme.of(context).primaryColor,
+              decoration: InputDecoration(
+                hintText: 'Username',
+                hintStyle: TextStyle(
+                  color: Colors.grey.shade500,
+                ),
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.grey.shade400,
+                  ),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.grey.shade400,
+                  ),
+                ),
+              ),
+            ),
+            Gaps.v36,
+            FormButton(isDisabled: _username.isEmpty),
+          ],
+        ),
+      ),
+    );
+  }
+}
