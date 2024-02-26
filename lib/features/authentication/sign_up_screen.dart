9c79d5010ad5a539d61d8e8d9d1cba1f56057aaa
jenna <shinysuh1992@gmail.com>
2024-02-26 12:54:53 +0900

    chore: redirection 파일 디렉토리 분리 common -> utils

--- lib/features/authentication/sign_up_screen.dart
+++ lib/features/authentication/sign_up_screen.dart
@@ -3,7 +3,7 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 import 'package:tiktok_clone/features/authentication/common/auth_button.dart';
-import 'package:tiktok_clone/features/authentication/common/common_redirection.dart';
+import 'package:tiktok_clone/utils/navigator_redirection.dart';
 import 'package:tiktok_clone/features/authentication/email_screen.dart';
 import 'package:tiktok_clone/features/authentication/login_screen.dart';
 import 'package:tiktok_clone/features/authentication/username_screen.dart';
