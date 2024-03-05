96e57e9d9eb4eecf2b32746d9a6a4b4fca4c7e8b
jenna <shinysuh1992@gmail.com>
2024-03-05 14:32:07 +0900

    feat: tutorialScreen 구현 완료

--- lib/features/onboarding/tutorial_screen.dart
+++ lib/features/onboarding/tutorial_screen.dart
@@ -1,6 +1,11 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/onboarding/widgets/tutorial_page.dart';
+
+enum Direction { right, left }
+
+// enum Page { first, second }
 
 class TutorialScreen extends StatefulWidget {
   const TutorialScreen({super.key});
@@ -10,108 +15,81 @@ class TutorialScreen extends StatefulWidget {
 }
 
 class _TutorialScreenState extends State<TutorialScreen> {
+  // Tap 이동 애니메이션 구현 TabBarView / AnimatedCrossFade
   /*
     TabBarView 와 TabPageSelector 가 둘 다
     DefaultTabController 안에 있어야
     원하는 방식 대로 사용 가능
   */
+
+  /*
+    AnimatedCrossFade
+        => 두 컴포넌트 사이에 fade-in / fade-out 을 매우 잘 표현
+  */
+
+  Direction _direction = Direction.right;
+
+  // Page _page = Page.first;
+  bool _isFirstPage = true;
+
+  void _onPanUpdate(DragUpdateDetails details) {
+    print(details);
+    // dx => direction to x-axis (x축 방향 - 플러스면 오른쪽으로 드래그(왼쪽으로 swipe))
+    setState(() {
+      if (details.delta.dx > 0) {
+        // to the right
+        _direction = Direction.right;
+      } else {
+        // to the left
+        _direction = Direction.left;
+      }
+    });
+  }
+
+  void _onPanEnd(DragEndDetails details) {
+    // _page = _direction == Direction.right ? Page.second : Page.first;
+    setState(() {
+      _isFirstPage = _direction == Direction.right;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 3, // 슬라이드로 적용하고자 하는 페이지 개수 (children 에 개수 맞춰 줘야함)
+    return GestureDetector(
+      onPanUpdate: _onPanUpdate,
+      onPanEnd: _onPanEnd,
       child: Scaffold(
-        body: const SafeArea(
-          child: TabBarView(
-            children: [
-              Padding(
-                padding: EdgeInsets.symmetric(
-                  horizontal: Sizes.size24,
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Gaps.v52,
-                    Text(
-                      'Watch cool videos!',
-                      style: TextStyle(
-                        fontSize: Sizes.size40,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Gaps.v16,
-                    Text(
-                      'Videos are personalized for you based on what you watch, like, and share.',
-                      style: TextStyle(
-                        fontSize: Sizes.size20,
-                      ),
-                    ),
-                  ],
-                ),
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: Sizes.size24),
+            child: AnimatedCrossFade(
+              crossFadeState: _isFirstPage
+                  ? CrossFadeState.showFirst
+                  : CrossFadeState.showSecond,
+              duration: const Duration(milliseconds: 300),
+              firstChild: const TutorialPage(
+                title: 'Watch cool videos!',
+                content:
+                    'Videos are personalized for you based on what you watch, like, and share.',
               ),
-              Padding(
-                padding: EdgeInsets.symmetric(
-                  horizontal: Sizes.size24,
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Gaps.v52,
-                    Text(
-                      'Follow the rules!',
-                      style: TextStyle(
-                        fontSize: Sizes.size40,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Gaps.v16,
-                    Text(
-                      'Videos are personalized for you based on what you watch, like, and share.',
-                      style: TextStyle(
-                        fontSize: Sizes.size20,
-                      ),
-                    ),
-                  ],
-                ),
+              secondChild: const TutorialPage(
+                title: 'Follow the rules!',
+                content: 'Take care of one another, please:)',
               ),
-              Padding(
-                padding: EdgeInsets.symmetric(
-                  horizontal: Sizes.size24,
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Gaps.v52,
-                    Text(
-                      'Enjoy the ride!',
-                      style: TextStyle(
-                        fontSize: Sizes.size40,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Gaps.v16,
-                    Text(
-                      'Videos are personalized for you based on what you watch, like, and share.',
-                      style: TextStyle(
-                        fontSize: Sizes.size20,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
+            ),
           ),
         ),
         bottomNavigationBar: BottomAppBar(
-          child: Container(
-            padding: const EdgeInsets.symmetric(vertical: Sizes.size48),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TabPageSelector(
-                  color: Colors.white,
-                  selectedColor: Theme.of(context).primaryColor,
-                )
-              ],
+          child: Padding(
+            padding: const EdgeInsets.all(Sizes.size24),
+            child: AnimatedOpacity(
+              duration: const Duration(milliseconds: 300),
+              opacity: _isFirstPage ? 0 : 1,
+              child: CupertinoButton(
+                onPressed: () {},
+                color: Theme.of(context).primaryColor,
+                child: const Text('Enter the App'),
+              ),
             ),
           ),
         ),
