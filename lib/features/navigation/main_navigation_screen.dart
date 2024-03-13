542879e745847d05366d69422ee0e5162d880898
jenna <shinysuh1992@gmail.com>
2024-03-13 17:47:11 +0900

    modify: 영상 scffold -> resizeToAvoidBottomInset=false

--- lib/features/navigation/main_navigation_screen.dart
+++ lib/features/navigation/main_navigation_screen.dart
@@ -56,6 +56,8 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // resizeToAvoidBottomInset : 키보드가 나타날 때 body 크기를 resize 할지 여부
+      resizeToAvoidBottomInset: false,
       backgroundColor: _selectedIndex == 0 ? Colors.black : Colors.white,
       body: Stack(
         children: [
