ba4e1f9dd062a1f766edce78c4857b0fad9c0278
jenna <shinysuh1992@gmail.com>
2024-04-15 12:54:39 +0900

    etc: router 정리

--- lib/router.dart
+++ lib/router.dart
@@ -1,43 +1,43 @@
 import 'package:go_router/go_router.dart';
-import 'package:tiktok_clone/features/authentication/email_screen.dart';
-import 'package:tiktok_clone/features/authentication/login_screen.dart';
-import 'package:tiktok_clone/features/authentication/sign_up_screen.dart';
-import 'package:tiktok_clone/features/authentication/username_screen.dart';
-import 'package:tiktok_clone/features/user/user_profile_screen.dart';
+import 'package:tiktok_clone/features/video/video_recording_screen.dart';
 
 final router = GoRouter(
   routes: [
     GoRoute(
-      name: SignUpScreen.routeName,
-      path: SignUpScreen.routeURL,
-      builder: (context, state) => const SignUpScreen(),
-      // nested routes
-      routes: [
-        GoRoute(
-          name: UsernameScreen.routeName,
-          path: UsernameScreen.routeURL,
-          builder: (context, state) => const UsernameScreen(),
-          routes: [
-            GoRoute(
-              name: EmailScreen.routeName,
-              path: EmailScreen.routeURL,
-              builder: (context, state) {
-                // extra 로 데이터 넘기기
-                final args = state.extra as EmailScreenArgs;
-                return EmailScreen(
-                  username: args.username,
-                );
-              },
-            ),
-          ],
-        ),
-      ],
+      path: '/',
+      builder: (context, state) => const VideoRecordingScreen(),
     ),
-    GoRoute(
-      name: LoginScreen.routeName,
-      path: LoginScreen.routeURL,
-      builder: (context, state) => const LoginScreen(),
-    ),
+    // GoRoute(
+    //   name: SignUpScreen.routeName,
+    //   path: SignUpScreen.routeURL,
+    //   builder: (context, state) => const SignUpScreen(),
+    //   // nested routes
+    //   routes: [
+    //     GoRoute(
+    //       name: UsernameScreen.routeName,
+    //       path: UsernameScreen.routeURL,
+    //       builder: (context, state) => const UsernameScreen(),
+    //       routes: [
+    //         GoRoute(
+    //           name: EmailScreen.routeName,
+    //           path: EmailScreen.routeURL,
+    //           builder: (context, state) {
+    //             // extra 로 데이터 넘기기
+    //             final args = state.extra as EmailScreenArgs;
+    //             return EmailScreen(
+    //               username: args.username,
+    //             );
+    //           },
+    //         ),
+    //       ],
+    //     ),
+    //   ],
+    // ),
+    // GoRoute(
+    //   name: LoginScreen.routeName,
+    //   path: LoginScreen.routeURL,
+    //   builder: (context, state) => const LoginScreen(),
+    // ),
     // GoRoute(
     //   name: UsernameScreen.routeName,
     //   path: UsernameScreen.routeURL,
@@ -52,16 +52,15 @@ final router = GoRouter(
     //           ),
     //       child: const UsernameScreen()),
     // ),
-
-    GoRoute(
-      path: '/user/:username',
-      builder: (context, state) {
-        // 쿼리파라미터로 데이터 넘기기
-        final username = state.params['username'] ?? '';
-        // e.g. '/user/:username?show=likes'
-        final param = state.queryParams['show'] ?? '';
-        return UserProfileScreen(username: username, show: param);
-      },
-    ),
+    // GoRoute(
+    //   path: '/user/:username',
+    //   builder: (context, state) {
+    //     // 쿼리파라미터로 데이터 넘기기
+    //     final username = state.params['username'] ?? '';
+    //     // e.g. '/user/:username?show=likes'
+    //     final param = state.queryParams['show'] ?? '';
+    //     return UserProfileScreen(username: username, show: param);
+    //   },
+    // ),
   ],
 );
