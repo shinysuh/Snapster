89de14a68bad6934029f3a9e95d2d6932cdcf46c
jenna <shinysuh1992@gmail.com>
2024-04-17 17:27:50 +0900

    feat: Video Preview 스크린 생성 && 비디오 저장 기능 구현(Gallery-saver)

---
+++ lib/features/video/video_preview_screen.dart
@@ -0,0 +1,74 @@
+import 'dart:io';
+
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:gallery_saver/gallery_saver.dart';
+import 'package:video_player/video_player.dart';
+
+class VideoPreviewScreen extends StatefulWidget {
+  final XFile video;
+
+  const VideoPreviewScreen({
+    super.key,
+    required this.video,
+  });
+
+  @override
+  State<VideoPreviewScreen> createState() => _VideoPreviewScreenState();
+}
+
+class _VideoPreviewScreenState extends State<VideoPreviewScreen> {
+  late final VideoPlayerController _videoPlayerController;
+
+  bool _isSaved = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _initVideo();
+  }
+
+  Future<void> _initVideo() async {
+    _videoPlayerController =
+        VideoPlayerController.file(File(widget.video.path));
+
+    await _videoPlayerController.initialize();
+    await _videoPlayerController.setLooping(true);
+    await _videoPlayerController.play();
+
+    // initialization 을 build 메소드가 알도록 하는 역할
+    setState(() {});
+  }
+
+  Future<void> _saveToGallery() async {
+    if (_isSaved) return;
+
+    await GallerySaver.saveVideo(widget.video.path, albumName: 'TikTok Clone');
+
+    setState(() {
+      _isSaved = true;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: const Text('Preview video'),
+        actions: [
+          IconButton(
+            onPressed: _saveToGallery,
+            icon: FaIcon(
+              _isSaved ? FontAwesomeIcons.check : FontAwesomeIcons.download,
+            ),
+          ),
+        ],
+      ),
+      body: _videoPlayerController.value.isInitialized
+          ? VideoPlayer(_videoPlayerController)
+          : null,
+    );
+  }
+}
