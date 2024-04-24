b8fbf2857a46bc60e36155bf67f9e80cef91d8e9
jenna <shinysuh1992@gmail.com>
2024-04-24 15:30:44 +0900

    feat: ChangeNotifier 실습 : AnimationBuilder 사용

--- lib/common/widgets/video_config/video_config.dart
+++ lib/common/widgets/video_config/video_config.dart
@@ -1,57 +1,71 @@
 import 'package:flutter/cupertino.dart';
 
-/* InheritedWidget 학습용 클래스 */
-class VideoConfigData extends InheritedWidget {
-  final bool autoMute;
-  final void Function() toggleMuted;
-
-  const VideoConfigData({
-    super.key,
-    required super.child,
-    required this.autoMute,
-    required this.toggleMuted,
-  });
-
-  static VideoConfigData of(BuildContext context) {
-    // dependOnInheritedWidgetOfExactType<클래스명> => context가 지정 클래스 타입의 inheritedWidget을 가져오게 함
-    return context.dependOnInheritedWidgetOfExactType<VideoConfigData>()!;
-  }
-
-  @override
-  bool updateShouldNotify(covariant InheritedWidget oldWidget) {
-    return true;
-  }
-}
-
-class VideoConfig extends StatefulWidget {
-  final Widget child;
-
-  const VideoConfig({
-    super.key,
-    required this.child,
-  });
-
-  @override
-  State<VideoConfig> createState() => _VideoConfigState();
-}
-
-class _VideoConfigState extends State<VideoConfig> {
-  // InheritedWidget 의 데이터를 업데이트하는 메커니즘은 없음
-  // StatefulWidget 와 연계해 이용
+/* ChangeNotifier 학습용 클래스 */
+class VideoConfig extends ChangeNotifier {
+  // 데이터 Listening 하는 부분에서 AnimatedBuilder 사용 OR 이벤트 리스너 추가
+  // 데이터 변경 시, AnimatedBuilder 부분만 rebuild 됨
   bool autoMute = false;
 
   void toggleMuted() {
-    setState(() {
-      autoMute = !autoMute;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return VideoConfigData(
-      autoMute: autoMute,
-      toggleMuted: toggleMuted,
-      child: widget.child,
-    );
+    autoMute = !autoMute;
+    notifyListeners();
   }
 }
+
+final videoConfig = VideoConfig();
+
+// /* InheritedWidget 학습용 클래스 */
+// class VideoConfigData extends InheritedWidget {
+//   final bool autoMute;
+//   final void Function() toggleMuted;
+//
+//   const VideoConfigData({
+//     super.key,
+//     required super.child,
+//     required this.autoMute,
+//     required this.toggleMuted,
+//   });
+//
+//   static VideoConfigData of(BuildContext context) {
+//     // dependOnInheritedWidgetOfExactType<클래스명> => context가 지정 클래스 타입의 inheritedWidget을 가져오게 함
+//     return context.dependOnInheritedWidgetOfExactType<VideoConfigData>()!;
+//   }
+//
+//   @override
+//   bool updateShouldNotify(covariant InheritedWidget oldWidget) {
+//     return true;
+//   }
+// }
+//
+// class VideoConfig extends StatefulWidget {
+//   final Widget child;
+//
+//   const VideoConfig({
+//     super.key,
+//     required this.child,
+//   });
+//
+//   @override
+//   State<VideoConfig> createState() => _VideoConfigState();
+// }
+//
+// class _VideoConfigState extends State<VideoConfig> {
+//   // InheritedWidget 의 데이터를 업데이트하는 메커니즘은 없음
+//   // StatefulWidget 와 연계해 이용
+//   bool autoMute = false;
+//
+//   void toggleMuted() {
+//     setState(() {
+//       autoMute = !autoMute;
+//     });
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return VideoConfigData(
+//       autoMute: autoMute,
+//       toggleMuted: toggleMuted,
+//       child: widget.child,
+//     );
+//   }
+// }
--- lib/features/settings/settings_screen.dart
+++ lib/features/settings/settings_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/common/widgets/video_config/video_config.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 import 'package:tiktok_clone/features/authentication/sign_up_screen.dart';
 import 'package:tiktok_clone/utils/navigator_redirection.dart';
@@ -102,6 +103,20 @@ class _SettingsScreenState extends State<SettingsScreen> {
               //   activeThumbImage: const AssetImage('assets/images/1.jpeg'),
               // ),
               // // SwitchListTile.adaptive => 마찬가지로 플랫폼 별 UI 형태 다름
+              AnimatedBuilder(
+                animation: videoConfig,
+                builder: (context, child) => SwitchListTile.adaptive(
+                  value: videoConfig.autoMute,
+                  onChanged: (value) => videoConfig.toggleMuted(),
+                  title: const Text('Auto mute videos'),
+                  subtitle: const Text(
+                    'Videos will be muted by default\n(currently not connected)',
+                    style: TextStyle(fontSize: Sizes.size12),
+                  ),
+                  activeColor: Theme.of(context).primaryColor,
+                  // activeThumbImage: const AssetImage('assets/images/1.jpeg'),
+                ),
+              ),
               // SwitchListTile.adaptive(
               //   value: VideoConfigData.of(context).autoMute,
               //   onChanged: (value) => VideoConfigData.of(context).toggleMuted(),
