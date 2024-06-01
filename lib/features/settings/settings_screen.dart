dbb3f2be64456048aa88586ada73bba2d591c3e7
jenna <shinysuh1992@gmail.com>
2024-06-01 11:58:39 +0900

    etc: settings_screen.dart 주석 정리

--- lib/features/settings/settings_screen.dart
+++ lib/features/settings/settings_screen.dart
@@ -9,8 +9,6 @@ import 'package:tiktok_clone/features/authentication/repositories/authentication
 import 'package:tiktok_clone/features/video/view_models/playback_config_view_model.dart';
 import 'package:tiktok_clone/utils/widgets/regulated_max_width.dart';
 
-// ConsumerWidget => Riverpod StatelessWidget
-// ConsumerStatefulWidget => Riverpod StatefulWidget
 class SettingsScreen extends ConsumerWidget {
   const SettingsScreen({super.key});
 
@@ -26,7 +24,6 @@ class SettingsScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return RegulatedMaxWidth(
-      // Localizations.override - locale => 언어 설정 강제 기능
       child: Localizations.override(
         context: context,
         // locale: const Locale('fr'),
@@ -37,90 +34,21 @@ class SettingsScreen extends ConsumerWidget {
           ),
           body: ListView(
             children: [
-              // Row(
-              //   mainAxisAlignment: MainAxisAlignment.center,
-              //   children: [
-              //     CupertinoSwitch(
-              //       value: _notifications,
-              //       onChanged: _onChangeNotifications,
-              //       activeColor: const Color(0xff75B1DF),
-              //     ),
-              //     Switch(
-              //       value: _notifications,
-              //       onChanged: _onChangeNotifications,
-              //       activeColor: Colors.deepPurple,
-              //       activeThumbImage: const AssetImage('assets/images/1.jpeg'),
-              //     ),
-              //     // Switch.adaptive 형태 => iOS (CupertinoSwitch) / 다른 플랫폼 (기본 switch)
-              //     Switch.adaptive(
-              //       value: _notifications,
-              //       onChanged: _onChangeNotifications,
-              //       activeColor: Colors.red,
-              //       activeThumbImage: const AssetImage('assets/images/1.jpeg'),
-              //     ),
-              //     Checkbox(
-              //       value: _notifications,
-              //       onChanged: _onChangeNotifications,
-              //       checkColor: Colors.white,
-              //       activeColor: const Color(0xffE694B0),
-              //     ),
-              //   ],
-              // ),
-              // CheckboxListTile(
-              //   value: _notifications,
-              //   onChanged: _onChangeNotifications,
-              //   title: const Text('Enable Notifications'),
-              //   checkColor: Colors.white,
-              //   activeColor: const Color(0xffFA8857),
-              // ),
-              // SwitchListTile(
-              //   value: _notifications,
-              //   onChanged: _onChangeNotifications,
-              //   title: const Text('Enable Notifications'),
-              //   activeColor: const Color(0xff267428),
-              //   activeThumbImage: const AssetImage('assets/images/1.jpeg'),
-              // ),
-              // // SwitchListTile.adaptive => 마찬가지로 플랫폼 별 UI 형태 다름
-
               SwitchListTile.adaptive(
                 value: ref.watch(playbackConfigProvider).muted,
-                // value: context.watch<PlaybackConfigViewModel>().muted,
                 onChanged: (value) =>
                     ref.read(playbackConfigProvider.notifier).setMuted(value),
-                // onChanged: (value) => context.read<PlaybackConfigViewModel>().setMuted(value),
                 title: const Text('Mute videos'),
                 activeColor: Theme.of(context).primaryColor,
-                // activeThumbImage: const AssetImage('assets/images/1.jpeg'),
               ),
               SwitchListTile.adaptive(
                 value: ref.watch(playbackConfigProvider).autoplay,
-                // value: context.watch<PlaybackConfigViewModel>().autoplay,
                 onChanged: (value) => ref
                     .read(playbackConfigProvider.notifier)
                     .setAutoplay(value),
-                // onChanged: (value) => context.read<PlaybackConfigViewModel>().setAutoplay(value),
                 title: const Text('Autoplay videos'),
                 activeColor: Theme.of(context).primaryColor,
-                // activeThumbImage: const AssetImage('assets/images/1.jpeg'),
               ),
-
-              // // provider
-              // SwitchListTile.adaptive(
-              //   // ValueNotifier
-              //   value: context.watch<VideoConfig>().isMuted,
-              //   onChanged: (value) =>
-              //       context.read<VideoConfig>().toggleIsMuted(),
-              //   // ChangeNotifier
-              //   // value: videoConfig.autoMute,
-              //   // onChanged: (value) => videoConfig.toggleMuted(),
-              //   title: const Text('Auto mute videos(Provider)'),
-              //   subtitle: const Text(
-              //     'Videos will be muted by default\n(currently not connected)',
-              //     style: TextStyle(fontSize: Sizes.size12),
-              //   ),
-              //   activeColor: Theme.of(context).primaryColor,
-              //   // activeThumbImage: const AssetImage('assets/images/1.jpeg'),
-              // ),
               ValueListenableBuilder(
                 valueListenable: screenModeConfig,
                 builder: (context, value, child) => SwitchListTile.adaptive(
@@ -130,67 +58,42 @@ class SettingsScreen extends ConsumerWidget {
                       screenModeConfig.value == ThemeMode.light
                           ? ThemeMode.dark
                           : ThemeMode.light,
-                  // ChangeNotifier
-                  // value: videoConfig.autoMute,
-                  // onChanged: (value) => videoConfig.toggleMuted(),
                   title: const Text('Dark Mode'),
                   activeColor: Theme.of(context).primaryColor,
-                  // activeThumbImage: const AssetImage('assets/images/1.jpeg'),
                 ),
               ),
               ValueListenableBuilder(
                 valueListenable: videoConfig,
                 builder: (context, value, child) => SwitchListTile.adaptive(
-                  // ValueNotifier
                   value: value,
                   onChanged: (value) => videoConfig.value = !videoConfig.value,
-                  // ChangeNotifier
-                  // value: videoConfig.autoMute,
-                  // onChanged: (value) => videoConfig.toggleMuted(),
                   title: const Text('Auto mute videos(ValueListenableBuilder)'),
                   subtitle: const Text(
                     'Videos will be muted by default\n(currently not connected)',
                     style: TextStyle(fontSize: Sizes.size12),
                   ),
                   activeColor: Theme.of(context).primaryColor,
-                  // activeThumbImage: const AssetImage('assets/images/1.jpeg'),
                 ),
               ),
               AnimatedBuilder(
                 animation: videoConfig,
                 builder: (context, child) => SwitchListTile.adaptive(
-                  // ValueNotifier
                   value: videoConfig.value,
                   onChanged: (value) => videoConfig.value = !videoConfig.value,
-                  // ChangeNotifier
-                  // value: videoConfig.autoMute,
-                  // onChanged: (value) => videoConfig.toggleMuted(),
                   title: const Text('Auto mute videos(AnimatedBuilder)'),
                   subtitle: const Text(
                     'Videos will be muted by default\n(currently not connected)',
                     style: TextStyle(fontSize: Sizes.size12),
                   ),
                   activeColor: Theme.of(context).primaryColor,
-                  // activeThumbImage: const AssetImage('assets/images/1.jpeg'),
                 ),
               ),
-              // SwitchListTile.adaptive(
-              //   value: VideoConfigData.of(context).autoMute,
-              //   onChanged: (value) => VideoConfigData.of(context).toggleMuted(),
-              //   title: const Text('Auto mute videos'),
-              //   subtitle: const Text(
-              //     'Videos will be muted by default',
-              //     style: TextStyle(fontSize: Sizes.size12),
-              //   ),
-              //   activeColor: Theme.of(context).primaryColor,
-              //   // activeThumbImage: const AssetImage('assets/images/1.jpeg'),
               // ),
               SwitchListTile.adaptive(
                 value: false,
                 onChanged: (value) {},
                 title: const Text('Enable Notifications'),
                 activeColor: Theme.of(context).primaryColor,
-                // activeThumbImage: const AssetImage('assets/images/1.jpeg'),
               ),
               CheckboxListTile.adaptive(
                 value: false,
@@ -213,7 +116,7 @@ class SettingsScreen extends ConsumerWidget {
                   );
                   if (kDebugMode) print(date);
 
-                  // if (!mounted) return;
+                  // if (!context.mounted) return;
                   final time = await showTimePicker(
                     context: context,
                     initialTime: TimeOfDay.now(),
@@ -355,49 +258,10 @@ class SettingsScreen extends ConsumerWidget {
                 ),
               ),
               const AboutListTile(
-                // AboutListTile : ListTile -> onTap -> showAboutDialog() 자동 적용된 것
                 applicationVersion: 'version 2.0',
               ),
             ],
           ),
-
-          // Column(
-          //   children: [
-          //     CupertinoActivityIndicator(
-          //       radius: 40,
-          //       // animating: false,
-          //     ),
-          //     CircularProgressIndicator(),
-          //     /* CircularProgressIndicator.adaptive => 현재 플랫폼에 따라 위 indecators 둘 중 하나의 형태로 출력 */
-          //     CircularProgressIndicator.adaptive(),
-          //   ],
-          // )
-
-          /* ListWheelScrollView => 원통형 스크롤 위젯 */
-          // ListWheelScrollView(
-          //   itemExtent: 200,
-          //   diameterRatio: 1.3,
-          //   offAxisFraction: -0.5,
-          //   // useMagnifier: true,
-          //   // magnification: 1.5,
-          //   children: [
-          //     for (var x in [1, 2, 3, 5, 5, 6, 2, 2, 43, 53, 6, 5, 353, 434, 234])
-          //       FractionallySizedBox(
-          //         widthFactor: 1,
-          //         child: Container(
-          //           color: Colors.teal,
-          //           alignment: Alignment.center,
-          //           child: const Text(
-          //             'Pick me!',
-          //             style: TextStyle(
-          //               color: Colors.white,
-          //               fontSize: Sizes.size36,
-          //             ),
-          //           ),
-          //         ),
-          //       )
-          //   ],
-          // ),
         ),
       ),
     );
