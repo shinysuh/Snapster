27bab8abf7db97931e3b3fb2995ee67e48e0e01a
jenna <shinysuh1992@gmail.com>
2024-06-11 15:25:36 +0900

    etc: 니코쌤 코드 테스트

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,14 +1,3 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
-// import {onRequest} from "firebase-functions/v2/https";
-// import * as logger from "firebase-functions/logger";
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 
@@ -59,7 +48,7 @@ export const onVideoCreated = functions.firestore
             
             await snapshot.ref.update({ thumbnailURL: file.publicUrl() });
             
-            /* 계속되는 컬럭션 미생성 및 콘솔 에러 로그 미출력으로 인해 VM 단으로 로직 이동 */
+            /* 계속되는 컬럭션 미생성 및 콘솔 에러 로그 미출력으로 인해 VM단(프론트단)으로 로직 이동 */
             // const db = admin.firestore();
             // console.log(`Updating user's video collection for uploaderUid: ${ video.uploaderUid }`);
             //
@@ -83,171 +72,42 @@ export const onVideoCreated = functions.firestore
     });
 
 
+// // 니코쌤 코드
 // import * as functions from "firebase-functions";
 // import * as admin from "firebase-admin";
-// import * as path from "path";
-// import * as os from "os";
-// import * as fs from "fs";
-// import * as ffmpeg from "fluent-ffmpeg";
-// import * as ffmpegPath from "ffmpeg-static";
 //
 // admin.initializeApp();
-//
-// // @ts-ignore
-// ffmpeg.setFfmpegPath(ffmpegPath);
 //
 // export const onVideoCreated = functions.firestore
 //     .document("videos/{videoId}")
 //     .onCreate(async (snapshot, context) => {
+//         const spawn = require("child-process-promise").spawn;
 //         const video = snapshot.data();
-//         if(!video) {
-//             console.error("No video data found in snapshot");
-//             return;
-//         }
-//
-//         const fileUrl = video.fileUrl;
-//         const bucketName = "tiktok-clone-jenn.appspot.com";
-//
-//         let filePath: string;
-//         try {
-//             const url = new URL(fileUrl);
-//             const pathname = decodeURIComponent(url.pathname);
-//             filePath = pathname.split('/o/')[1].split('?')[0];
-//         } catch(error) {
-//             console.error("Error parsing file URL:", error);
-//             return;
-//         }
-//
-//         const tempLocalFile = path.join(os.tmpdir(), `${ snapshot.id }.mp4`);
-//         const tempLocalThumbFile = path.join(os.tmpdir(), `${ snapshot.id }.jpg`);
-//
-//         const bucket = admin.storage().bucket(bucketName);
-//         try {
-//             await bucket.file(filePath).download({ destination: tempLocalFile });
-//         } catch(error) {
-//             console.error("Error downloading video file:", error);
-//             return;
-//         }
-//
-//         return new Promise<void>((resolve, reject) => {
-//             ffmpeg(tempLocalFile)
-//                 .screenshots({
-//                     timestamps: [ '1' ],
-//                     filename: `${ snapshot.id }.jpg`,
-//                     folder: os.tmpdir(),
-//                     size: '150x?'
-//                 })
-//                 .on('end', async () => {
-//                     try {
-//                         await bucket.upload(tempLocalThumbFile, {
-//                             destination: `thumbnails/${ snapshot.id }.jpg`
-//                         });
-//
-//                         fs.unlinkSync(tempLocalFile);
-//                         fs.unlinkSync(tempLocalThumbFile);
-//
-//                         const thumbnailURL = `https://storage.googleapis.com/${ bucketName }/thumbnails/${ snapshot.id }.jpg`;
-//                         await snapshot.ref.update({ thumbnailURL });
-//                         resolve();
-//                     } catch(error) {
-//                         console.error("Error uploading thumbnail or updating Firestore:", error);
-//                         reject(error);
-//                     }
-//                 })
-//                 .on('error', (error) => {
-//                     console.error("Error creating thumbnail:", error);
-//                     reject(error);
-//                 });
+//         await spawn("ffmpeg", [
+//             "-i",
+//             video.fileUrl,
+//             "-ss",
+//             "00:00:01.000",
+//             "-vframes",
+//             "1",
+//             "-vf",
+//             "scale=150:-1",
+//             `/tmp/${ snapshot.id }.jpg`,
+//         ]);
+//         const storage = admin.storage();
+//         const [ file, _ ] = await storage.bucket().upload(`/tmp/${ snapshot.id }.jpg`, {
+//             destination: `thumbnails/${ snapshot.id }.jpg`,
 //         });
-//     });
-//
-
-
-///////
-// import * as functions from "firebase-functions";
-// import * as admin from "firebase-admin";
-// import * as path from "path";
-// import * as os from "os";
-// import * as fs from "fs";
-// import * as ffmpeg from "fluent-ffmpeg";
-// import * as ffmpegPath from "ffmpeg-static";
-//
-// admin.initializeApp();
-//
-// // @ts-ignore
-// ffmpeg.setFfmpegPath(ffmpegPath);
-//
-// export const onVideoCreated = functions.firestore
-//     .document("videos/{videoId}")
-//     .onCreate(async (snapshot, context) => {
-//         const video = snapshot.data();
-//         if(!video) {
-//             console.log("No video data found in snapshot");
-//             return;
-//         }
-//
-//         console.log('############### video:', video.fileUrl, video);
-//
-//         // Firebase Storage URL에서 파일 경로 추출
-//         const fileUrl = video.fileUrl;
-//         const bucketName = "tiktok-clone-jenn.appspot.com";
-//
-//         let filePath: string;
-//         try {
-//             const url = new URL(fileUrl);
-//             const pathname = decodeURIComponent(url.pathname);
-//             filePath = pathname.split('/o/')[1].split('?')[0];
-//         } catch(error) {
-//             console.error("Error parsing file URL:", error);
-//             return;
-//         }
-//
-//         const tempLocalFile = path.join(os.tmpdir(), `${ snapshot.id }.mp4`);
-//         const tempLocalThumbFile = path.join(os.tmpdir(), `${ snapshot.id }.jpg`);
-//
-//         // 비디오 파일 다운로드
-//         const bucket = admin.storage().bucket(bucketName);
-//         try {
-//             console.log(`Downloading video file from gs://${ bucketName }/${ filePath } to ${ tempLocalFile }`);
-//             await bucket.file(filePath).download({ destination: tempLocalFile });
-//             console.log("Video file downloaded successfully");
-//         } catch(error) {
-//             console.error("Error downloading video file:", error);
-//             return;
-//         }
-//
-//         return new Promise<void>((resolve, reject) => {
-//             ffmpeg(tempLocalFile)
-//                 .screenshots({
-//                     timestamps: [ '1' ],
-//                     filename: `${ snapshot.id }.jpg`,
-//                     folder: os.tmpdir(),
-//                     size: '150x?'
-//                 })
-//                 .on('end', async () => {
-//                     console.log("Thumbnail created successfully");
-//                     try {
-//                         console.log(`Uploading thumbnail to thumbnails/${ snapshot.id }.jpg`);
-//                         await bucket.upload(tempLocalThumbFile, {
-//                             destination: `thumbnails/${ snapshot.id }.jpg`
-//                         });
-//                         console.log("Thumbnail uploaded successfully");
-//
-//                         fs.unlinkSync(tempLocalFile);
-//                         fs.unlinkSync(tempLocalThumbFile);
-//
-//                         const thumbnailURL = `https://storage.googleapis.com/${ bucketName }/thumbnails/${ snapshot.id }.jpg`;
-//                         console.log(`Updating Firestore document with thumbnail URL: ${ thumbnailURL }`);
-//                         await snapshot.ref.update({ thumbnailURL });
-//                         resolve();
-//                     } catch(error) {
-//                         console.error("Error uploading thumbnail or updating Firestore:", error);
-//                         reject(error);
-//                     }
-//                 })
-//                 .on('error', (error) => {
-//                     console.error("Error creating thumbnail:", error);
-//                     reject(error);
-//                 });
-//         });
+//         await file.makePublic();
+//         await snapshot.ref.update({ thumbnailUrl: file.publicUrl() });
+//         const db = admin.firestore();
+//         await db
+//             .collection("users")
+//             .doc(video.uploaderUid)
+//             .collection("videos")
+//             .doc(snapshot.id)
+//             .set({
+//                 thumbnailUrl: file.publicUrl(),
+//                 videoId: snapshot.id,
+//             });
 //     });
