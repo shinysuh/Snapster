42abb1bfba5bb605eaf838017c8c4a43d0e95574
jenna <shinysuh1992@gmail.com>
2025-04-07 15:46:59 +0900

    feat: User + UserProfile 엔티티 생성

---
+++ backend/src/main/java/com/jenna/snapster/domain/user/entity/User.java
@@ -0,0 +1,32 @@
+package com.jenna.snapster.domain.user.entity;
+
+import com.jenna.snapster.domain.userprofile.entity.UserProfile;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String oauthProvider;       // kakao, google, apple
+    private String oauthId;
+
+    private String email;
+    private String username;
+
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private UserProfile profile;
+
+
+}
---
+++ backend/src/main/java/com/jenna/snapster/domain/userprofile/entity/UserProfile.java
@@ -0,0 +1,34 @@
+package com.jenna.snapster.domain.userprofile.entity;
+
+import com.jenna.snapster.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "user_profiles")
+public class UserProfile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @MapsId
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private String name;
+    private String bio;
+    private String link;
+    private LocalDate birthday;
+    private boolean hasAvatar;
+}
