3727b7422099e2d8fa0ef43942674bc42edf9286
jenna <shinysuh1992@gmail.com>
2025-04-07 15:46:17 +0900

    feat: oAuth + jwt configuration 구현

---
+++ backend/src/main/java/com/jenna/snapster/core/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.jenna.snapster.core.config;
+
+import com.jenna.snapster.core.security.oauth.OAuth2SuccessHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final OAuth2SuccessHandler oAuth2SuccessHandler;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(auth ->
+                auth.requestMatchers(
+                        "/",
+                        "/login/**",
+                        "/oauth2/**"
+                    ).permitAll()
+                    .anyRequest()
+                    .authenticated()
+            ).oauth2Login(oauth -> oauth.successHandler(oAuth2SuccessHandler));
+
+        return http.build();
+    }
+}
---
+++ backend/src/main/java/com/jenna/snapster/core/security/oauth/OAuth2SuccessHandler.java
@@ -0,0 +1,41 @@
+package com.jenna.snapster.core.security.oauth;
+
+import com.jenna.snapster.core.security.jwt.JwtProvider;
+import com.jenna.snapster.domain.user.entity.User;
+import com.jenna.snapster.domain.user.service.UserService;
+import com.nimbusds.common.contenttype.ContentType;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
+
+    private final UserService userService;
+    private final JwtProvider jwtProvider;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request,
+                                        HttpServletResponse response,
+                                        Authentication authentication) throws IOException {
+        OAuth2AuthenticationToken token = (OAuth2AuthenticationToken) authentication;
+        OAuth2User oAuth2User = token.getPrincipal();
+        String provider = token.getAuthorizedClientRegistrationId();    // "kakao"
+
+        User user = userService.processOAuthUser(provider, oAuth2User);
+
+        String accessToken = jwtProvider.generateAccessToken(user);
+
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.setContentType(ContentType.APPLICATION_JSON.getType());
+        response.getWriter().write("{\"accessToken\":\"" + accessToken + "\"}");
+    }
+}
