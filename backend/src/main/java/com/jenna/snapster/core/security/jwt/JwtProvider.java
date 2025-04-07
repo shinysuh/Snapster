3fe926b93b1ec73d210a957a8dac8a64d678449a
jenna <shinysuh1992@gmail.com>
2025-04-07 15:45:35 +0900

    feat: jwt provider 클래스 구현

---
+++ backend/src/main/java/com/jenna/snapster/core/security/jwt/JwtProvider.java
@@ -0,0 +1,49 @@
+package com.jenna.snapster.core.security.jwt;
+
+import com.jenna.snapster.domain.user.entity.User;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtProvider {
+
+    private final long EXPIRATION = 1000L * 60 * 30;        // 30분
+
+    @Value(value = "${jwt.secret}")
+    private String secret;
+
+    private final Key secretKey = new SecretKeySpec(
+        secret.getBytes(StandardCharsets.UTF_8),
+        SignatureAlgorithm.HS256.getJcaName()
+    );
+
+    public String generateAccessToken(User user) {
+        return Jwts.builder()
+            .setSubject(String.valueOf(user.getId()))
+            .claim("username", user.getUsername())
+            .claim("email", user.getEmail())
+            .setIssuedAt(new Date())
+            .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION))
+            .signWith(secretKey)
+            .compact();
+    }
+
+    public Long getUserIdFromToken(String token) {
+        Claims claims = Jwts.parserBuilder()
+            .setSigningKey(secretKey)
+            .build()
+            .parseClaimsJws(token)
+            .getBody();
+
+        return Long.valueOf(claims.getSubject());
+
+    }
+}
