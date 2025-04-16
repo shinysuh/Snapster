69093faea5c595629928cbdb29f6a2d52ede6d2e
jenna <shinysuh1992@gmail.com>
2025-04-16 15:18:35 +0900

    feat(google): 구글 oauth2 연동

--- backend/src/main/java/com/jenna/snapster/domain/oauth/service/impl/GoogleOAuthUserServiceImpl.java
+++ backend/src/main/java/com/jenna/snapster/domain/oauth/service/impl/GoogleOAuthUserServiceImpl.java
@@ -2,18 +2,64 @@ package com.jenna.snapster.domain.oauth.service.impl;
 
 import com.jenna.snapster.domain.oauth.service.OAuthUserService;
 import com.jenna.snapster.domain.user.entity.User;
+import com.jenna.snapster.domain.user.entity.UserProfile;
+import com.jenna.snapster.domain.user.repository.UserProfileRepository;
+import com.jenna.snapster.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
+import java.util.Optional;
 
+
+@Slf4j
 @Service("GoogleOAuthUserService")
 @RequiredArgsConstructor
 public class GoogleOAuthUserServiceImpl implements OAuthUserService {
+
+    private final UserRepository userRepository;
+    private final UserProfileRepository userProfileRepository;
+
     @Override
     public User processOAuthUser(String provider, OAuth2User oAuth2User) {
-        System.out.println("************ GOOGLE SERVICE CALLED ************");
+        log.info("************ GOOGLE SERVICE CALLED ************");
 
-        return null;
+        Map<String, Object> attributes = oAuth2User.getAttributes();
+
+        String oauthId = (String) attributes.get("sub");
+        String email = (String) attributes.get("email");
+        String name = (String) attributes.get("name");
+
+        // 기존 유저 정보 확인
+        Optional<User> userOpt = userRepository.findByOauthProviderAndOauthId(provider, oauthId);
+        log.info("************ User {} ************", userOpt.isPresent() ? "Exists" : "Does Not Exist");
+        if (userOpt.isPresent()) return userOpt.get();
+
+
+// 새 유저 등록
+        User user = User.builder()
+            .oauthProvider(provider)
+            .oauthId(oauthId)
+            .email(email)
+            .username(name)
+            .build();
+
+        // profile 연결
+        UserProfile userProfile = UserProfile.builder()
+            .user(user)
+            .name(user.getUsername())
+            .bio("")
+            .link("")
+            .birthday(null)
+            .hasAvatar(false)
+            .build();
+
+        user.setProfile(userProfile);
+
+        // 유저 저장 (profile 같이 저장됨)
+        user = userRepository.save(user);
+        return user;
     }
 }
