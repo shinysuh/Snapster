5da9e2421567ec299dca4c6a7040d5495ad62edb
jenna <shinysuh1992@gmail.com>
2025-04-27 19:21:36 +0900

    fix: 토큰 없을 경우 무한 로딩 오류 fix

--- snapster_app/lib/features/authentication/views/splash_screen.dart
+++ snapster_app/lib/features/authentication/views/splash_screen.dart
@@ -1,14 +1,58 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:snapster_app/common/widgets/navigation/views/main_navigation_screen.dart';
+import 'package:snapster_app/features/authentication/providers/http_auth_provider.dart';
+import 'package:snapster_app/features/authentication/views/sign_up/sign_up_screen.dart';
+import 'package:snapster_app/utils/navigator_redirection.dart';
 
-class Splashscreen extends ConsumerWidget {
+class Splashscreen extends ConsumerStatefulWidget {
   static const String routeName = 'splash';
   static const String routeURL = '/splash';
 
   const Splashscreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return const Scaffold(body: Center(child: CircularProgressIndicator()));
+  ConsumerState<Splashscreen> createState() => _SplashscreenState();
+}
+
+class _SplashscreenState extends ConsumerState<Splashscreen>
+    with WidgetsBindingObserver {
+  late final StreamSubscription _sub;
+
+  @override
+  void initState() {
+    super.initState();
+    initApp();
+  }
+
+  @override
+  void dispose() {
+    _sub.cancel();
+    super.dispose();
+  }
+
+  Future<void> initApp() async {
+    await Future.delayed(const Duration(milliseconds: 100)); // ⏳ 렌더링 시간 확보
+
+    final user = ref.read(authRepositoryProvider).currentUser;
+
+    if (!mounted) return;
+    final route = user != null
+        ? MainNavigationScreen.homeRouteURL
+        : SignUpScreen.routeURL;
+
+    goRouteReplacementRoute(
+      context: context,
+      routeURL: route,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: CircularProgressIndicator()),
+    );
   }
 }
