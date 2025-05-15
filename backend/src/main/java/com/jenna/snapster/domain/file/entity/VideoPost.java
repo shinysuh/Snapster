98aacb1f5c35ce7c0841ea8b14078b9a2fd62e3d
jenna <shinysuh1992@gmail.com>
2025-05-16 01:30:12 +0900

    feat: 태그 trim 처리

--- backend/src/main/java/com/jenna/snapster/domain/file/entity/VideoPost.java
+++ backend/src/main/java/com/jenna/snapster/domain/file/entity/VideoPost.java
@@ -9,8 +9,10 @@ import org.apache.commons.lang3.StringUtils;
 
 import java.time.LocalDateTime;
 import java.util.Arrays;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Entity
 @Table(name = "video_posts")
@@ -62,7 +64,12 @@ public class VideoPost {
     @PreUpdate
     private void updateTagsString() {
         if (Objects.nonNull(tagList) && !tagList.isEmpty()) {
-            this.tags = String.join(",", tagList);
+            this.tags = String.join(",",
+                tagList.stream()
+                    .map(String::trim)
+                    .filter(StringUtils::isNotEmpty)
+                    .collect(Collectors.toCollection(LinkedHashSet::new))
+            );
         }
     }
 }
