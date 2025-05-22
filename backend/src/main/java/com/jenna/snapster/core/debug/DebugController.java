cdd5c34a355f5a925f6fde8534b1370e3944188e
jenna <shinysuh1992@gmail.com>
2025-05-22 18:10:00 +0900

    refactor: 디버그 컨트롤러 수정

--- backend/src/main/java/com/jenna/snapster/core/debug/DebugController.java
+++ backend/src/main/java/com/jenna/snapster/core/debug/DebugController.java
@@ -2,15 +2,25 @@ package com.jenna.snapster.core.debug;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @Slf4j
 @RestController
+@RequestMapping("/api/debug")
 public class DebugController {
 
-    @GetMapping("/api/debug/headers")
-    public void headers(HttpServletRequest request) {
+    @GetMapping("/headers")
+    public ResponseEntity<?> headers(HttpServletRequest request) {
         log.info("X-Forwarded-Proto: {}", request.getHeader("X-Forwarded-Proto"));
+        return ResponseEntity.ok(
+            Map.of(
+                "X-Forwarded-Proto", request.getHeader("X-Forwarded-Proto")
+            )
+        );
     }
 }
