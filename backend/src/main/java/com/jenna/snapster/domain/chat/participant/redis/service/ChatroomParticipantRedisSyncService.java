a68b5d2bedf51ec4cea0190d83b98ddb6d02e682
jenna <shinysuh1992@gmail.com>
2025-06-21 17:00:57 +0900

    feat: 앱 초기화 시 미사용 채팅방 삭제 로직 적용

--- backend/src/main/java/com/jenna/snapster/domain/chat/participant/redis/service/ChatroomParticipantRedisSyncService.java
+++ backend/src/main/java/com/jenna/snapster/domain/chat/participant/redis/service/ChatroomParticipantRedisSyncService.java
@@ -1,15 +1,18 @@
 package com.jenna.snapster.domain.chat.participant.redis.service;
 
+import com.jenna.snapster.domain.chat.chatroom.repository.ChatroomRepository;
 import com.jenna.snapster.domain.chat.participant.entity.ChatroomParticipant;
 import com.jenna.snapster.domain.chat.participant.redis.repository.ChatroomRedisRepository;
 import com.jenna.snapster.domain.chat.participant.repository.ChatroomParticipantRepository;
-import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Set;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -19,26 +22,36 @@ public class ChatroomParticipantRedisSyncService {
 
     private final ChatroomRedisRepository chatroomRedisRepository;
     private final ChatroomParticipantRepository participantRepository;
+    private final ChatroomRepository chatroomRepository;
 
-    @PostConstruct
+    @EventListener(ApplicationReadyEvent.class)
+    @Async
     public void initChatroomParticipantsSyncToRedis() {
         log.info("[Redis Init] 채팅방 참여자 정보를 Redis에 적재 시작");
 
         List<ChatroomParticipant> allParticipants = participantRepository.findAll();
 
-        Set<Long> chatroomIds = allParticipants.stream()
-            .map(cp -> cp.getId().getUserId())
-            .collect(Collectors.toSet());
+        Map<Long, List<ChatroomParticipant>> chatroomMap = allParticipants.stream()
+            .collect(Collectors.groupingBy(cp -> cp.getId().getChatroomId()));
 
-        for (Long chatroomId : chatroomIds) {
-            List<Long> participants = allParticipants.stream()
-                .filter(cp -> cp.getId().getChatroomId().equals(chatroomId))
+        for (Map.Entry<Long, List<ChatroomParticipant>> entry : chatroomMap.entrySet()) {
+            Long chatroomId = entry.getKey();
+            List<Long> participants = entry.getValue().stream()
                 .map(cp -> cp.getId().getUserId())
                 .toList();
 
+            // 참여자 없는 방 삭제
+            this.removeChatroomIfEmpty(chatroomId, participants);
+            // 참여자 정보 redis 적재
             chatroomRedisRepository.addParticipants(chatroomId, participants);
         }
 
         log.info("[Redis Init] 채팅방 참여자 정보를 Redis에 적재 완료");
+    }
+
+    private void removeChatroomIfEmpty(Long chatroomId, List<Long> participants) {
+        if (participants.isEmpty()) {
+            chatroomRepository.deleteById(chatroomId);
+        }
     }
 }
