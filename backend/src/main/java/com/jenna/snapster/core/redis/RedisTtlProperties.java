79db6803774c74fa3a42cacf459e7992f0064cd5
jenna <shinysuh1992@gmail.com>
2025-06-26 13:41:49 +0900

    fix: redis ttl 세팅 오류 fix

--- backend/src/main/java/com/jenna/snapster/core/redis/RedisTtlProperties.java
+++ backend/src/main/java/com/jenna/snapster/core/redis/RedisTtlProperties.java
@@ -4,7 +4,7 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @Data
-@ConfigurationProperties(prefix = "redis.chat")
+@ConfigurationProperties(prefix = "redis.ttl")
 public class RedisTtlProperties {
 
     private long feed;
